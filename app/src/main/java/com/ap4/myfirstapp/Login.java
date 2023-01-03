008dd989245ff35cece625a82d8240e4dd3374bc
Phenix-145 <mathis.pause501@gmail.com>
2023-01-03 15:47:08 +0100

    test_user

--- app/src/main/java/com/ap4/myfirstapp/Login.java
+++ app/src/main/java/com/ap4/myfirstapp/Login.java
@@ -3,6 +3,7 @@ package com.ap4.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import static android.content.Intent.ACTION_VIEW;
 
@@ -10,13 +11,14 @@ import android.annotation.SuppressLint;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.View;
 
-public class Login extends AppCompatActivity {
-
+public class Login extends AppCompatActivity implements View.OnClickListener {
+    EditText user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +27,18 @@ public class Login extends AppCompatActivity {
         btn.setOnClickListener(view -> {
             Intent intent = new Intent( Login.this, MainActivity.class );
             startActivity(intent);
-
         });
+
+        user = (EditText) findViewById (R.id.user);
+        //password = (EditText) findViewById (R.id.password);
+
     }
+        @Override
+        public void onClick(View v) {
+            // TODO Auto-generated method stub
+            String chaine = user.getText().toString();
+            Log.v("Essai",chaine);
+        }
+
+
 }
