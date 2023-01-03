7c303d05ca460870713ea15fcd30320f67073189
khara-ns <khara-ns@protonmail.com>
2023-01-03 15:30:46 +0100

    Login page + home menu

--- app/src/main/java/com/ap4/myfirstapp/Login.java
+++ app/src/main/java/com/ap4/myfirstapp/Login.java
@@ -3,6 +3,17 @@ package com.ap4.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import static android.content.Intent.ACTION_VIEW;
+
+import android.annotation.SuppressLint;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +21,11 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button btn = (Button) findViewById(R.id.menuhome);
+        btn.setOnClickListener(view -> {
+            Intent intent = new Intent( Login.this, MainActivity.class );
+            startActivity(intent);
+
+        });
     }
 }
