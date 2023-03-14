e7f804d084d1380f182b17b30703099c6f55dc3e
khara-ns <khara-ns@protonmail.com>
2023-03-14 14:30:20 +0100

    Debut d'integration de la requete a la bdd avec le QR CODE et Integration de l'objet produit

---
+++ app/src/main/java/com/ap4/myfirstapp/ProduitUniqueObjet.java
@@ -0,0 +1,23 @@
+package com.ap4.myfirstapp;
+
+public class ProduitUniqueObjet {
+
+    static String produitNom;
+    static String produitRef;
+    static int produitPrix;
+    static String produitDesc;
+    static String produitRayon;
+    static String produitFournisseur;
+    static String produitLieuStockage;
+
+    public ProduitUniqueObjet(String nom , String ref , int prix , String description , String rayon , String fournisseur , String lieuStockage) {
+        System.out.println("Le :" + nom );
+        produitNom = nom;
+        produitRef = ref;
+        produitPrix = prix;
+        produitDesc = description;
+        produitRayon = rayon;
+        produitFournisseur = fournisseur;
+        produitLieuStockage = lieuStockage;
+    }
+}
--- app/src/main/java/com/ap4/myfirstapp/QrCode.java
+++ app/src/main/java/com/ap4/myfirstapp/QrCode.java
@@ -1,6 +1,7 @@
 package com.ap4.myfirstapp;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -10,6 +11,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.Headers;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 public class QrCode extends AppCompatActivity {
     Button btn_scan;
@@ -46,4 +62,57 @@ public class QrCode extends AppCompatActivity {
             }).show();
         }
     });
+    private void grabProduit(String uuid) {
+        String url = "https://site.btsap3.tk/SiteFichier/ap3/api/apiProduitSingle.php";
+
+        final OkHttpClient client = new OkHttpClient();
+        RequestBody requestBody = new FormBody.Builder()
+                .add("uuid", uuid)
+                .build();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .post(requestBody)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try (ResponseBody responseBody = response.body()) {
+                    if (!response.isSuccessful())
+                        throw new IOException("Unexpected code " + response);
+
+                    Headers responseHeaders = response.headers();
+                    for (int i = 0, size = responseHeaders.size(); i < size; i++) {
+                        System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
+                    }
+
+                    String responseBodyS = responseBody.string();
+                    System.out.println("TEST REPONCE" + responseBodyS);
+
+                    try {
+                        JSONObject jsonO = new JSONObject(responseBodyS);
+                        ProduitUniqueObjet MonProduitUnique = new ProduitUniqueObjet("","",2,"","","","");
+                        boolean success = (boolean) jsonO.get("success");
+                        System.out.println("TEST BOOLEAN" + success);
+
+                        if (success == true) {
+                            Intent intent = new Intent(QrCode.this, Page_magasin.class);
+                            startActivity(intent);
+                        }
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+
+        });
+    }
 }
